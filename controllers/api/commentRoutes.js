567cdc15f3f4e419c439c08fc9bf51e319239a46
walterunderwood <walonsounderwood@gmail.com>
2022-05-10 13:44:55 -0500

    Creating comment routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,46 @@ const router = require('express').Router()
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            include: User
+        });
+        const comments = commentData.map((comment) => comment.get({ plain: true }));
+        console.log(comments);
+        res.render('thiswillbemyhandlebarinthefuture', { comments, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body, user_id: req.session.user_id,
+        });
+        res.json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const oldComment = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        })
+        res.status(200).json(oldComment);
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
+
+
 
 
 
