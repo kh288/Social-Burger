88a377ba89fffb12abd4fdadbdb7d3f1b10aebcc
walterunderwood <walonsounderwood@gmail.com>
2022-05-11 08:27:31 -0500

    Creating Homepage and Dashboard handlebars

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const { User } = require('../../models');
-const withAuth = require('../../utils');
+const withAuth = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
     try {
