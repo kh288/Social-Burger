19d85505cacb7b76550d58abd4a12fed42f6bfd0
walterunderwood <walonsounderwood@gmail.com>
2022-05-12 20:34:21 -0500

    Implement the new technology  AOS

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -35,5 +35,43 @@ router.get('/create', withAuth, (req, res) => {
     res.render('create', { layout: 'dashboard', });
 });
 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id',
+            'title',
+            'ingredients',
+            'date_created'
+        ],
+        include: [{
+            model: User,
+            attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'text', 'burger_id', 'user_id', 'date_created'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            const burgers = dbPostData.get({ plain: true });
+            res.render('edit-burger', { burgers, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
 
 module.exports = router;
