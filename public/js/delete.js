6546aa4d93fa0c1981387c75b9ad3db40815363f
walterunderwood <walonsounderwood@gmail.com>
2022-05-13 10:00:35 -0500

    Updating edit-burgers function

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,44 +1,44 @@
-// const deleteFormHandler = async (event) => {
-//     event.preventDefault();
-
-//     const burgerId = document.querySelector('.burgerInput').value.trim()
-//     console.log(burgerId);
-
-//     const response = await fetch(`/api/burger/${id}`, {
-//         method: 'DELETE',
-//         body: JSON.stringify({
-//             burger_id: burgerId
-//         }),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     });
-//     if (response.ok) {
-//         document.location.replace('/dashboard/');
-//     } else {
-//         alert(response.statusText);
-//     }
-// }
-
-
-
 const delButtonHandler = async (event) => {
+    event.preventDefault();
 
-    if (event.target.hasAttribute('data-id')) {
-        const id = event.target.getAttribute('data-id');
+    const id = document.querySelector('.deleteBtn').value.trim()
+    console.log(id);
 
-        const response = await fetch(`/api/burger/${id}`, {
-            method: 'DELETE',
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard/');
-        } else {
-            alert('Failed to delete Burger');
+    const response = await fetch(`/api/burger/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+            burger_id: id
+        }),
+        headers: {
+            'Content-Type': 'application/json'
         }
+    });
+    if (response.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(response.statusText);
+        document.location.replace('/dashboard/');
     }
-};
+}
+
+
+
+// const delButtonHandler = async (event) => {
+
+//     if (event.target.hasAttribute('data-id')) {
+//         const id = event.target.getAttribute('data-id');
+
+//         const response = await fetch(`/api/burger/${id}`, {
+//             method: 'DELETE',
+//         });
+//         if (response.ok) {
+//             document.location.replace('/dashboard/');
+//         } else {
+//             alert('Failed to delete Burger');
+//         }
+//     }
+// };
 
 document
     .querySelector('.deleteForm')
-    .addEventListener('click', delButtonHandler);
+    .addEventListener('submit', delButtonHandler);
