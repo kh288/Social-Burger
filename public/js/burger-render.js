d9427c52bea5ab35219b29cae700168825d04ddd
Kevin Hernandez <kevin.hernandez288@gmail.com>
2022-05-12 21:03:38 -0700

    Making new Ingredient Model

--- public/js/burger-render.js
+++ public/js/burger-render.js
@@ -1,7 +1,7 @@
 // const rawInput = document.querySelector('.burger-render');
 let getBurgerQuery;
 let queryArray = [];
-// const burgerArray = [];
+let burgerArray = [];
 
 function getBurgerSection() {
     getBurgerQuery = document.getElementsByTagName('section');
@@ -11,17 +11,49 @@ function getBurgerSection() {
         // getBurgerQuery[i].querySelector(getBurgerQuery[i].attributes.id.value);
         queryArray.push(getBurgerQuery[i].attributes.id.value);
     }
+    // console.log(queryArray);
 }
-getBurgerSection();
-let burger = getBurgerQuery[1].innerHTML.split(' ').join('\n').split('\n');
-burger = burger.filter(function(space) {
-    return space.trim() != '';
-});
 
-console.log(burger);
+getBurgerSection();
+
+function returnBurgerArray() {
+    // var burger;
+    const response = await fetch('/', {
+        method: 'GET',
+        
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    // console.log(response);
+    // console.log(burger);
+    // .split(' ').join('\n').split('\n')
+    for(var i = 0; i < getBurgerQuery.length; i++) {
+        var burger = getBurgerQuery[i].innerText;
+        // burger = burger.filter(function(space) {
+        //     return space.trim() != '';
+        // });
+        // burger[2] is ACTUAL burger ID in SQL DB
+        burgerArray.push(burger);
+
+        burgerArray[i] = burgerArray[i].split(' ');
+        // if (burgerArray[j].includes(',')) {
+        //     burgerArray[j].pop();
+        // }
+        // for (var j = 0; j < burgerArray[i].length; j++) {
+        //     if (burgerArray[j] === "23,20,17,14") {
+        //         burgerArray[j].pop();
+        //     }
+        // }
+    }
+    // burgerArray = burgerArray.filter(isNaN);
+}
+
+returnBurgerArray();
+
+console.log(burgerArray);
+
 function renderBurger() {
-    
-    burgerArray = getBurgerQuery[1].innerHTML.split(',');
+    burgerArray = getBurgerQuery[1].innerText.split(',');
 
     console.log('Created burgerArray: ' + burgerArray);
     console.log('Created burgerId: ' + burgerId);
